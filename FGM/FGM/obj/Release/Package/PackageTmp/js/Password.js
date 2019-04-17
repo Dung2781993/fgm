e380b09b90e1b36173d506b96fea3b636eefde43
Dung2781993@gmail.com <Dung2781993@gmail.com>
2019-04-17 12:17:04 +1000

    Update password protection

---
+++ FGM/FGM/obj/Release/Package/PackageTmp/js/Password.js
@@ -0,0 +1,9 @@
+﻿var userInputPassword;
+var password = "TEST07";
+
+while(userInputPassword !== (password.toLowerCase())) {
+    UserInputPassword = prompt('Enter Password');
+    if ((UserInputPassword.toLowerCase()) === (password.toLowerCase())) {
+        break;
+    }
+}
