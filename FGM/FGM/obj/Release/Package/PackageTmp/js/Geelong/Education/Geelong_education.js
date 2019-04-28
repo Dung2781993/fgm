1aaa0749e3597f70c2cef8c1a135349484ddb538
Dung2781993@gmail.com <Dung2781993@gmail.com>
2019-04-28 17:26:52 +1000

    Update advance colleges and Tertiary description

--- FGM/FGM/obj/Release/Package/PackageTmp/js/Geelong/Education/Geelong_education.js
+++ FGM/FGM/obj/Release/Package/PackageTmp/js/Geelong/Education/Geelong_education.js
@@ -34,6 +34,7 @@ $('#educationSearch').click(function () {
         }
     }
     if (select === "Secondary School") {
+        //Insert Secondary Description
         addDescription(select);
         for (var i = 0; i < 5; i++) {
             var secondaryName = geelong_secondary_school.name[i];
@@ -43,7 +44,9 @@ $('#educationSearch').click(function () {
             secondaryMarker.bindPopup(secondaryName).openPopup();
         }
     }
-    if (select === "Advanced School") {
+    if (select === "Advanced Colleges") {
+        //Insert Advanced Description
+        addDescription(select);
         for (var k = 0; k < 5; k++) {
             var advancedName = geelong_advanced_colleges.name[k];
             var advancedLat = geelong_advanced_colleges.lat[k];
@@ -53,6 +56,8 @@ $('#educationSearch').click(function () {
         }
     }
     if (select === "Tertiary") {
+        //Insert Tertiary Description
+        addDescription(select);
         for (var h = 0; h < 5; h++) {
             var tertiaryName = geelong_tertiary.name[h];
             var tertiaryLat = geelong_tertiary.lat[h];
@@ -61,50 +66,44 @@ $('#educationSearch').click(function () {
             tertiaryMarker.bindPopup(tertiaryName).openPopup();
         }
     }
-    if (select === "Special Need") {
-        for (var g = 0; g < 5; g++) {
-            var SpecialName = geelong_special_needs_school.name[g];
-            var SpecialLat = geelong_special_needs_school.lat[g];
-            var SpecialLong = geelong_special_needs_school.long[g];
-            var SpecialMarker = L.marker([SpecialLat, SpecialLong]).addTo(map);
-            SpecialMarker.bindPopup(SpecialName).openPopup();
-        }
-    }
 });
 
 function addDescription(type) {
     if (type === "Primary School") {
         $('#description').append(
             '<ul id="descriptionDetail">' +
+            '<p class="mapDetail">' +
+            'The primary schools offer education from year K to 6. All the schools focus on nurturing the primary years of children with values, friendship and education. Below is the description of top 5 primary schools in Geelong.' +
+            '</p>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Geelong East is a Government owned, co - educational primary school established in 1857.' +
-            ' For more information: <a href="For more information: http://geeastps.vic.edu.au/">http://geeastps.vic.edu.au/</a>' +
+            ' For more information: <a href="http://geeastps.vic.edu.au/">http://geeastps.vic.edu.au/</a>' +
             '</p >' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'South Geelong is Government owned , co-educational primary school established in 1879.' +
-            ' For more information: <a href="For more information: http://gsps.vic.edu.au">http://gsps.vic.edu.au</a>' +
+            ' For more information: <a href="http://gsps.vic.edu.au">http://gsps.vic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Moolap South Geelong is Government owned, co-educational primary school established in 1879.' +
-            ' For more information: <a href="For more information: http://gsps.vic.edu.au">http://gsps.vic.edu.au</a>' +
+            ' For more information: <a href="http://gsps.vic.edu.au">http://gsps.vic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Wallington is Government owned, co-educational primary school established in 1866.' +
-            ' For more information: <a href="For more information: https://www.wallington-ps.vic.edu.au">https://www.wallington-ps.vic.edu.au</a>' +
+            ' For more information: <a href="https://www.wallington-ps.vic.edu.au">https://www.wallington-ps.vic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Montpellier Government owned, co-educational primary school established in 1969.' +
-            ' For more information: <a href="For more information: http://www.montpellierps.vic.edu.au">http://www.montpellierps.vic.edu.au (edited)</a>' +
+            ' For more information: <a href="http://www.montpellierps.vic.edu.au">http://www.montpellierps.vic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '</ul>'
@@ -112,42 +111,97 @@ function addDescription(type) {
     }
     if (type === "Secondary School") {
         $('#description').append(
-            '<p class="mapDetail">' +
-            'The secondary schools offer education from year 7 to 12. All the secondary schools in Geelong are co-educational and offer a wide range of co-curricular activities focused on sports and music. Below is the description of top 5 secondary schools in Geelong.'+
-            '</p>'+
             '<ul id="descriptionDetail">' +
+            '<p class="mapDetail">' +
+            'The secondary schools offer education from year 7 to 12. All the secondary schools in Geelong are co-educational and offer a wide range of co-curricular activities focused on sports and music. Below is the description of top 5 secondary schools in Geelong.' +
+            '</p>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Geelong Grammar is a private, co-educational secondary school which was established in 1855.' +
-            ' For more information: <a href="For more information: https://www.ggs.vic.edu.au">https://www.ggs.vic.edu.au</a>' +
+            ' For more information: <a href="https://www.ggs.vic.edu.au">https://www.ggs.vic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Kardinia International is a private, co-educational junior and senior school which was established in 1855.' +
-            ' For more information: <a href="For more information: https://www.kardinia.vic.edu.au/about/welcome/">https://www.kardinia.vic.edu.au/about/welcome/</a>' +
+            ' For more information: <a href="https://www.kardinia.vic.edu.au/about/welcome/">https://www.kardinia.vic.edu.au/about/welcome/</a>' +
             '</p >' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Clonard College is a Kildare Education Ministries, co-educational Catholic School which was established in 1956.' +
-            ' For more information: <a href="For more information: http://web.clonard.catholic.edu.au">http://web.clonard.catholic.edu.au</a>' +
+            ' For more information: <a href="http://web.clonard.catholic.edu.au">http://web.clonard.catholic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'The Geelong College is an independent, co-educational, day and boarding school which was established in 1886.' +
-            ' For more information: <a href="For more information: https://www.geelongcollege.vic.edu.au">https://www.geelongcollege.vic.edu.au</a>' +
+            ' For more information: <a href="https://www.geelongcollege.vic.edu.au">https://www.geelongcollege.vic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '<li>' +
             '<p class="mapDetail">' +
             'Sacred Heart College is a private Roman Catholic, only girls, secondary school which was established in 1861.' +
-            ' For more information: <a href="For more information: http://www.shcgeelong.catholic.edu.au">http://www.shcgeelong.catholic.edu.au</a>' +
+            ' For more information: <a href="http://www.shcgeelong.catholic.edu.au">http://www.shcgeelong.catholic.edu.au</a>' +
             '</p >' +
             '</li>' +
             '</ul>'
         );
     }
+    if (type === "Tertiary") {
+        $('#description').append(
+            '<ul id="descriptionDetail">' +
+            '<p class="mapDetail">' +
+            'More than 27,000 students are enrolled in tertiary schools in Geelong. The two main education hubs are Deakin University and Gordon Institute.' +
+            '</p>' +
+            '<li>' +
+            '<p class="mapDetail">' +
+            'Deakin University is a public university which was established in 1974. It offer both undergraduate and postgraduate courses. It has four campuses out of which two are in Geelong in Waterfront and Waurn Ponds. It is one of the fastest growing research universities in Australia.' +
+            ' For more information: <a href="https://www.deakin.edu.au">https://www.deakin.edu.au</a>' +
+            '</p >' +
+            '</li>' +
+            '<li>' +
+            '<p class="mapDetail">' +
+            'Gordon Institute is a TAFE institute ie. technical and further education and was established in 1887. It has three campuses in Geelong and East Geelong located in Moorabool and Boundary Road. The institution also provides facility of an English learning centre.' +
+            ' For more information: <a href="https://www.thegordon.edu.au">https://www.thegordon.edu.au</a>' +
+            '</p >' +
+            '</li>' +
+            '</ul>'
+        );
+    }
+    if (type === "Advanced Colleges") {
+        $('#description').append(
+            '<ul id="descriptionDetail">' +
+            '<p class="mapDetail">' +
+            'Advanced colleges provide flexible, hands-on training to students in different domains such as agriculture, nursing and fitness so that they can utilise those skills in their work. Some of the institutions also impart knowledge related to religion and scriptures as well.' +
+            '</p>' +
+            '<li>' +
+            '<p class="mapDetail">' +
+            'Reformed Theological College is a co-educational advanced college, founded in 1954 and focuses on education related to understanding of biblical scriptures.' +
+            ' For more information: <a href="http://www.rtc.edu.au">http://www.rtc.edu.au</a>' +
+            '</p >' +
+            '</li>' +
+            '<li>' +
+            '<p class="mapDetail">' +
+            'Marcus Oldham College is a private institute founded in 1962 which focus on farm management and agribusiness courses.' +
+            ' For more information: <a href="https://marcusoldham.vic.edu.au">https://marcusoldham.vic.edu.au</a>' +
+            '</p >' +
+            '</li>' +
+            '<li>' +
+            '<p class="mapDetail">' +
+            'Victorian Fitness Academy is a private institute that provides diploma courses in sports management.' +
+            ' For more information: <a href="https://vfalearning.vic.edu.au">https://vfalearning.vic.edu.au</a>' +
+            '</p >' +
+            '</li>' +
+            '<li>' +
+            '<p class="mapDetail">' +
+            'Centre of Excellence is a hairdressing and beauty college which was established in 1981.' +
+            ' For more information: <a href="http://www.thecentreofexcellence.com.au">http://www.thecentreofexcellence.com.au</a>' +
+            '</p >' +
+            '</li>' +
+            '</ul>'
+        );
+    }
+
 }
