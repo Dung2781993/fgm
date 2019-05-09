2cfe538dad58ffb71a805d59af63fa3d82d59cd1
Dung2781993@gmail.com <Dung2781993@gmail.com>
2019-05-09 23:02:45 +1000

    Update Geelong View

--- FGM/FGM/obj/Release/Package/PackageTmp/js/Geelong/Education/Geelong_education.js
+++ FGM/FGM/obj/Release/Package/PackageTmp/js/Geelong/Education/Geelong_education.js
@@ -1,5 +1,6 @@
 ﻿//Filtering education facilities and display on the map 
 //Initial Map
+
 var mapOptions = {
     center: [-38.14711, 144.36069],
     zoom: 10
@@ -13,13 +14,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiZmdtbW9uYXNoIiwiYSI6ImNqdTBybzEwcTF1ZG40NHJ6a3g4ZGZzZW8ifQ.jwIjG6Q5cJyzy87lU8BmvQ'
 }).addTo(map);
 
-//Display all educations in Geelong
 for (var g = 0; g < 20; g++) {
     var allEducationName = geelong_all_eduction.name[g];
     var allEducationLat = geelong_all_eduction.lat[g];
     var allEducationLong = geelong_all_eduction.long[g];
-    var primaryMarker = L.marker([allEducationLat, allEducationLong]).addTo(map);
-    primaryMarker.bindPopup(allEducationName).openPopup();
+    var allEducationMarker = L.marker([allEducationLat, allEducationLong]).addTo(map);
+    allEducationMarker.bindPopup(allEducationName).openPopup();
 }
 
 
@@ -28,17 +28,32 @@ $('#educationSearch').click(function () {
     //Remove Description
     $('#description').find('#descriptionDetail').remove();
 
+    var mapOptions = {
+        center: [-38.14711, 144.36069],
+        zoom: 10
+    };
+
     var select = $('#educationList :selected').text();
     var container = L.DomUtil.get('mapEducation');
     if (container !== null) { container._leaflet_id = null; }
 
+
+    var map = new L.map('mapEducation', mapOptions);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        minZoom: 8,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiZmdtbW9uYXNoIiwiYSI6ImNqdTBybzEwcTF1ZG40NHJ6a3g4ZGZzZW8ifQ.jwIjG6Q5cJyzy87lU8BmvQ'
+    }).addTo(map);
+
+    
     if (select === "All Education Facilities") {
         for (var g = 0; g < 20; g++) {
             var allEducationName = geelong_all_eduction.name[g];
             var allEducationLat = geelong_all_eduction.lat[g];
             var allEducationLong = geelong_all_eduction.long[g];
-            var primaryMarker = L.marker([allEducationLat, allEducationLong]).addTo(map);
-            primaryMarker.bindPopup(allEducationName).openPopup();
+            var allEducationMarker = L.marker([allEducationLat, allEducationLong]).addTo(map);
+            allEducationMarker.bindPopup(allEducationName).openPopup();
         }
     }
     if (select === "Primary School") {
--- FGM/FGM/obj/Release/Package/PackageTmp/js/Geelong/Healthcare/Geelong_healthcare.js
+++ FGM/FGM/obj/Release/Package/PackageTmp/js/Geelong/Healthcare/Geelong_healthcare.js
@@ -20,8 +20,8 @@ for (var g = 0; g < 11; g++) {
     var allHealthName = geelong_all_healthcare.name[g];
     var allHealthLat = geelong_all_healthcare.lat[g];
     var allHealthLong = geelong_all_healthcare.long[g];
-    var primaryMarker = L.marker([allHealthLat, allHealthLong]).addTo(map);
-    primaryMarker.bindPopup(allHealthName).openPopup();
+    var allHealthMarker = L.marker([allHealthLat, allHealthLong]).addTo(map);
+    allHealthMarker.bindPopup(allHealthName).openPopup();
 }
 
 $('#healthSearch').click(function () {
@@ -31,6 +31,30 @@ $('#healthSearch').click(function () {
     var select = $('#healthList :selected').text();
     var container = L.DomUtil.get('mapHospital');
     if (container !== null) { container._leaflet_id = null; }
+
+    //Initial Map
+    var mapOptions = {
+        center: [-38.14711, 144.36069],
+        zoom: 10
+    };
+
+    var map = new L.map('mapHospital', mapOptions);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        minZoom: 8,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiZmdtbW9uYXNoIiwiYSI6ImNqdTBybzEwcTF1ZG40NHJ6a3g4ZGZzZW8ifQ.jwIjG6Q5cJyzy87lU8BmvQ'
+    }).addTo(map);
+
+    if (select === "All Healthcare Service") {
+        for (var g = 0; g < 11; g++) {
+            var allHealthName = geelong_all_healthcare.name[g];
+            var allHealthLat = geelong_all_healthcare.lat[g];
+            var allHealthLong = geelong_all_healthcare.long[g];
+            var allHealthMarker = L.marker([allHealthLat, allHealthLong]).addTo(map);
+            allHealthMarker.bindPopup(allHealthName).openPopup();
+        }
+    }
 
     if (select === "Emergency Hospital") {
         //Insert primary Description
